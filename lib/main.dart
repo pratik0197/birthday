1a0e8f407d8cde041a4cbabc9e2a29db85fe20fa
PRATIK <priyadarshanpratik123@gmail.com>
2020-09-17 00:51:09 +0530

    Added AppIcon to iamrich App

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() {
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey.shade900,
+          backgroundColor: Colors.blueGrey.shade900
         ),
         body: Center(
           child: Image(
-            image: NetworkImage("https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753"),
+            image: AssetImage('./images/diamond.jpg'),
           ),
         ),
     ),
